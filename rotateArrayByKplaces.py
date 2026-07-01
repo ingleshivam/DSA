00da563e0bf09d167ca63e2c728be9749210abd3
ingleshivam <ingleshivam@gmail.com>
2026-07-02 03:34:31 +0530

    Refactor rotateArrayByKPlace function to use three reversal steps for array rotation

--- rotateArrayByKplaces.py
+++ rotateArrayByKplaces.py
@@ -1,12 +1,28 @@
 #rotateArrayByKplaces.py
 
+# arr= [1,2,3,4,5,6,7]
+# k = 3
+
+# def rotateArrayByKPlace(arr,k):
+#     length = len(arr)
+#     k%=length
+#     arr[:] = arr[length-k:] + arr[:length-k]
+
+# result = rotateArrayByKPlace(arr, k)
+# print("Final Result Array : ",arr)
+
+def rotateArrayByKPlace(arr,left, right):
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left +=1
+        right -=1
 arr= [1,2,3,4,5,6,7]
 k = 3
+n = len(arr)
+print(n)
+rotateArrayByKPlace(arr,n-k,n-1)
+rotateArrayByKPlace(arr,0,n-1-k)
+rotateArrayByKPlace(arr,0,n-1)
 
-def rotateArrayByKPlace(arr,k):
-    length = len(arr)
-    k%=length
-    arr[:] = arr[length-k:] + arr[:length-k]
-
-result = rotateArrayByKPlace(arr, k)
-print("Final Result Array : ",arr)
+print(arr)
+    
