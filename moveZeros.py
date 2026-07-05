aebc7ff5e97ce4e2dd17311547e9b24ebf2d4818
ingleshivam <ingleshivam@gmail.com>
2026-07-05 14:02:57 +0530

    Refactor moveZeros function to improve zero shifting logic and enhance readability

--- moveZeros.py
+++ moveZeros.py
@@ -3,15 +3,19 @@
 def moveZeros(arr):
     n = len(arr)
     i = 0
-    j = 1
-    while j<n:
-        if arr[i] == 0 and arr[j] !=0:
-            arr[i], arr[j] = arr[j], arr[i]
-            i+=1
-            j+=1
-        else:
-            j+=1
+    j = 0
+    while i<n:
+        if arr[i] == 0:
+            j=i+1            
+            while j < n and arr[j] == 0:
+                j += 1
+            if j<n:
+                arr[i],arr[j] = arr[j], arr[i]
+        i+=1    
+                    
 arr = [1,2,0,4,3,0,0,3,5,1]
 moveZeros(arr)
-print(arr)
+print("Result Array : ",arr)
+
+
     
