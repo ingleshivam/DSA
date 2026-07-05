8a489f99e40262eea61aa90ed83a374107a25f83
ingleshivam <ingleshivam@gmail.com>
2026-07-05 14:12:51 +0530

    Add implementation of linear search function to find target element in an array

---
+++ linearSearch.py
@@ -0,0 +1,7 @@
+def findTargetElementUsingLinearSearch(arr, target):
+    for num in arr:
+        if num == target:
+            print(f"Target element found at position {arr.index(num)}")
+arr = [5,3,9,8,1,6,4,-10,-100]
+target = 4
+position = findTargetElementUsingLinearSearch(arr, target)
